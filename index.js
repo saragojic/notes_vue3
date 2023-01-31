44f14f72ec3c599831fd9f4424557efffcda6757
saragojic <sa20b004@technikum-wien.at>
2023-01-31 22:21:22 +0100

    Step 9: extend data model

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const { createApp } = Vue;
 createApp({
+    // Model
     data() {
         return {
-            notes: ["Note 1", "Note 2", "Note 3"],
+            notes: [
+                { id: "676c9ba771", title: "Title 1", text: "ToDo 1" },
+                { id: "dc19d1538f", title: "Title 2", text: "ToDo 2" },
+                { id: "fd8c75b4fb", title: "Title 3", text: "ToDo 2" },
+            ],
+            title: null,
             text: null,
         };
     },
+    // Controller
     methods: {
         add() {
             if (this.text) {
