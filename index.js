86baa7e822eba5155f58342b6d9ece0d485af698
saragojic <sa20b004@technikum-wien.at>
2023-01-31 21:42:07 +0100

    Step 4: basic implement

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const { createApp } = Vue;
-createApp({}).mount("#app");
+createApp({
+    data() {
+        return {
+            notes: ["Note 1", "Note 2", "Note 3"],
+            text: null,
+        };
+    },
+    methods: {
+        handleClick() {
+            if (this.text) {
+                this.notes.push(this.text);
+            }
+        },
+    },
+}).mount("#app");
