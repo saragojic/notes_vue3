22d27063123f45ba1e96be89c3119a0bab5c1521
saragojic <sa20b004@technikum-wien.at>
2023-02-02 21:39:32 +0100

    Step 19: implement service worker functionality - control

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ createApp({
             notes: [],
             title: "",
             text: "",
+            worker: null,
+            updateAvailable: false,
         };
     },
     // Controller
@@ -21,6 +23,42 @@ createApp({
             const position = this.notes.findIndex((note) => note.id === id);
             this.notes.splice(position, 1);
         },
+        registerServiceWorker() {
+            if ("serviceWorker" in navigator) {
+                let refreshing;
+                navigator.serviceWorker.addEventListener("controllerchange", () => {
+                    if (refreshing) return;
+                    window.location.reload();
+                    regfreshing = true;
+                });
+                navigator.serviceWorker
+                    .register("/notes-vue-3/sw.js", { scope: "/notes-vue-3/" })
+                    .then((registration) => this.handleRegistration(registration))
+                    .catch((error) => console.log("Service Worker registration failed!", error));
+            }
+        },
+        handleRegistration(registration) {
+            const thiz = this;
+            registration.addEventListener("updatefound", function () {
+                if (registration.installing) {
+                    thiz.worker = registration.installing;
+                    thiz.worker.addEventListener("statechange", function () {
+                        if (thiz.worker.state === "installed") {
+                            thiz.updateAvailable = true;
+                        }
+                    });
+                } else if (registration.waiting) {
+                    thiz.worker = registration.waiting;
+                    if (thiz.worker.state === "installed") {
+                        thiz.updateAvailable = true;
+                    }
+                }
+            });
+        },
+        update() {
+            this.worker.postMessage({ action: "skipWaiting" });
+            this.updateAvailable = false;
+        },
     },
     watch: {
         notes: {
@@ -32,7 +70,7 @@ createApp({
     },
     created() {
         this.notes = JSON.parse(localStorage.getItem("notes")) || [];
-        registerServiceWorker();
+        this.registerServiceWorker();
     },
 }).mount("#app");
 
@@ -45,43 +83,4 @@ function generateId(title, text, length = 10) {
     return CryptoJS.SHA256(title + text + new Date())
         .toString()
         .substring(0, length);
-}
-
-function registerServiceWorker() {
-    if ("serviceWorker" in navigator) {
-        let refreshing;
-        navigator.serviceWorker.addEventListener("controllerchange", () => {
-            if (refreshing) return;
-            window.location.reload();
-            regfreshing = true;
-        });
-        navigator.serviceWorker
-            .register("/notes_vue3/sw.js", { scope: "/notes-vue-3/" })
-            .then((registration) => handleRegistration(registration))
-            .catch((error) => console.log("Service Worker registration failed!", error));
-    }
-}
-
-function handleRegistration(registration) {
-    registration.addEventListener("updatefound", function () {
-        if (registration.installing) {
-            const worker = registration.installing;
-            worker.addEventListener("statechange", function () {
-                if (worker.state === "installed") {
-                    handleUpdate(worker);
-                }
-            });
-        } else if (registration.waiting) {
-            const worker = registration.waiting;
-            if (worker.state === "installed") {
-                handleUpdate(worker);
-            }
-        }
-    });
-}
-
-function handleUpdate(worker) {
-    if (navigator.serviceWorker.controller) {
-        worker.postMessage({ action: "skipWaiting" });
-    }
 }
