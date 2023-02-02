e8814ed6d4fdd00ad06cdff66179707386d9f150
saragojic <sa20b004@technikum-wien.at>
2023-02-02 21:22:57 +0100

    Step 16: add a service worker

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ createApp({
         add() {
             if (this.title || this.text) {
                 this.notes.push(createNote(this.title, this.text));
-
                 this.title = "";
                 this.text = "";
             }
@@ -33,6 +32,7 @@ createApp({
     },
     created() {
         this.notes = JSON.parse(localStorage.getItem("notes")) || [];
+        registerServiceWorker();
     },
 }).mount("#app");
 
@@ -45,4 +45,13 @@ function generateId(title, text, length = 10) {
     return CryptoJS.SHA256(title + text + new Date())
         .toString()
         .substring(0, length);
+}
+
+function registerServiceWorker() {
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker
+            .register("/notes-vue-3/sw.js", { scope: "/notes-vue-3/" })
+            .then((registration) => console.log("Service Worker registered!", registration))
+            .catch((error) => console.log("Service Worker registration failed!", error));
+    }
 }
