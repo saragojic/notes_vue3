1d0b4a49a5cf0677baa4cedaa706e29c61eeb13e
saragojic <sa20b004@technikum-wien.at>
2023-02-02 20:41:05 +0100

    Step 11: implement new model

--- index.js
+++ index.js
@@ -15,14 +15,26 @@ createApp({
     // Controller
     methods: {
         add() {
-            if (this.text) {
-                this.notes.push(this.text);
+            if (this.title || this.text) {
+                this.notes.push(createNote(this.title, this.text));
+                this.title = "";
                 this.text = "";
-                this.$refs.input.focus();
             }
         },
-        handleClickLIItem(position) {
+        del(id) {
+            const position = this.notes.findIndex((note) => note.id === id);
             this.notes.splice(position, 1);
         },
     },
 }).mount("#app");
+
+function createNote(title, text) {
+    const id = generateId(title, text);
+    return { id, title, text };
+}
+
+function generateId(title, text, length = 10) {
+    return CryptoJS.SHA256(title + text + new Date())
+        .toString()
+        .substring(0, length);
+}
